3d042558554c6733567bbcb81398b617b6278019
ArchitectAK <ankitdroiddeveloper@gmail.com>
2023-04-11 19:44:05 +0800

    add instantiate method

--- contract.js
+++ contract.js
@@ -2,12 +2,21 @@
 
 import { Contract } from 'fabric-contract-api';
 class CertNetContract extends Contract {
-  
+
   constructor() {
     super('certnet');
 
   }
 
+  // Instantiate
+  async instantiate(ctx){
+    console.log('Chaincode deployed successfully')
+  }
+
+  // Create Student
+  // Get Student
+  // Issue Certificate
+  // Verify Certificate
 }
 
 module.exports = CertNetContract;
