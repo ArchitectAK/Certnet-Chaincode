10f599540c37119d7aa56c22b9944635fe3e1796
ArchitectAK <ankitdroiddeveloper@gmail.com>
2023-04-12 14:50:34 +0800

    Add createStudent method

--- contract.js
+++ contract.js
@@ -16,8 +16,24 @@ class CertNetContract extends Contract {
   // Create Student
   async createStudent(ctx, studentId, name, email) {
     // Composite key
-    const studentKey = ctx.stub.createCompositeKey('certnet.student', )
+    const studentKey = ctx.stub.createCompositeKey('certnet.student', [studentId])
+    const newStudent = {
+      docType: 'student',
+      studentId,
+      name,
+      email,
+      school: ctx.clientIdentity.getID(),
+      createdAt: ctx.stub.getTxTimestamp(),
+      updatedAt: ctx.stub.getTxTimestamp(),
+    }
+    const studentBuffer = Buffer.from(JSON.stringify(newStudent));
+    
+    await ctx.stub.putState(studentKey, studentBuffer);
+
+    return newStudent;
+
   }
+  
   // Get Student
   // Issue Certificate
   // Verify Certificate
